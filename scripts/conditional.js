6734135970e5d18aebc7ebd6cc57b5abef2848ad
Saras-git <saraswathisubbaiah02@gmail.com>
2026-02-22 18:04:08 +0530

    learn conditional statements

---
+++ scripts/conditional.js
@@ -0,0 +1,16 @@
+let person;
+let age=20;
+//if
+if(age > 18){
+    person='adult';
+}
+
+//else if
+else if(age>12){
+    person='teen';
+}
+//else
+else{
+    person='child';
+}
+console.log(person);
